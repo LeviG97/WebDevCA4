459a207cd7fb40b74c5519f764edd0fa086b0ba3
LeviG97 <98537056+LeviG97@users.noreply.github.com>
2023-03-21 16:49:35 +0000

    commit, unfinished CarAPIService. slide9.

--- carsales-nosql/src/app/services/car-api.service.ts
+++ carsales-nosql/src/app/services/car-api.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs';
+import { tap } from 'rxjs';
+import { ICar } from '../Interfaces/cars';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarApiService {
-
-  constructor() { }
+  
+  constructor() {}
 }
