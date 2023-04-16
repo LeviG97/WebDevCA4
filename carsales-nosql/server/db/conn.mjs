97367b4d31f654e0159737850e095eb8e1ee6a5c
LeviG97 <98537056+LeviG97@users.noreply.github.com>
2023-04-16 16:53:09 +0100

    commit to movefiles and add mongodb to conn.mjs

--- carsales-nosql/server/db/conn.mjs
+++ carsales-nosql/server/db/conn.mjs
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const connectionString = process.env.ATLAS_URI || "";
+const connectionString = process.env.ATLAS_URI || "mongodb+srv://LeviG:1l1aOOu8nmQB62SY@wp1.hb2iuqs.mongodb.net/?retryWrites=true&w=majority";
 
 const client = new MongoClient(connectionString);
 
